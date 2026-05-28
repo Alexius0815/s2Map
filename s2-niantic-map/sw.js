f6069996b9c6e8c62a9b3877804d55e0b72b8327
alex <alex@1hofmann.de>
2026-05-28 16:47:32 +0200

    Move help toggle to top right

--- s2-niantic-map/sw.js
+++ s2-niantic-map/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "s2-maps-shell-v10";
+const CACHE_NAME = "s2-maps-shell-v11";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=waypoints-8",
-  "./app.js?v=waypoints-8",
+  "./styles.css?v=waypoints-9",
+  "./app.js?v=waypoints-9",
   "./icon.svg",
   "./manifest.webmanifest",
   "./impressum.html",
