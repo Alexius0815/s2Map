34240fcf5be9ca9aa4dd52d89e0f527f81994e8e
alex <alex@1hofmann.de>
2026-05-28 16:36:06 +0200

    Improve mobile start layout

--- s2-niantic-map/sw.js
+++ s2-niantic-map/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "s2-maps-shell-v9";
+const CACHE_NAME = "s2-maps-shell-v10";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=waypoints-7",
-  "./app.js?v=waypoints-7",
+  "./styles.css?v=waypoints-8",
+  "./app.js?v=waypoints-8",
   "./icon.svg",
   "./manifest.webmanifest",
   "./impressum.html",
