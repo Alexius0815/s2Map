2ff4f42d40b23d51795714561d61d542bd239f00
alex <alex@1hofmann.de>
2026-05-28 17:01:30 +0200

    Rename cells button to S2 Grid

--- s2-niantic-map/sw.js
+++ s2-niantic-map/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "s2-maps-shell-v12";
+const CACHE_NAME = "s2-maps-shell-v13";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=waypoints-10",
-  "./app.js?v=waypoints-10",
+  "./styles.css?v=waypoints-11",
+  "./app.js?v=waypoints-11",
   "./icon.svg",
   "./manifest.webmanifest",
   "./impressum.html",
