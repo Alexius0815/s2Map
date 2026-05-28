05067328cf9357834371b77184b038e17a106d54
alex <alex@1hofmann.de>
2026-05-28 16:59:53 +0200

    Shrink map controls and align menu buttons

--- s2-niantic-map/sw.js
+++ s2-niantic-map/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "s2-maps-shell-v11";
+const CACHE_NAME = "s2-maps-shell-v12";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=waypoints-9",
-  "./app.js?v=waypoints-9",
+  "./styles.css?v=waypoints-10",
+  "./app.js?v=waypoints-10",
   "./icon.svg",
   "./manifest.webmanifest",
   "./impressum.html",
