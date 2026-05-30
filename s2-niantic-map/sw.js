1c41e8be3a4595efd3ab114cbd9b25bdaf0c93d6
alex <alex@1hofmann.de>
2026-05-30 09:12:47 +0200

    Improve inline changelog layout

--- s2-niantic-map/sw.js
+++ s2-niantic-map/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "s2-maps-shell-v45";
+const CACHE_NAME = "s2-maps-shell-v46";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=waypoints-43",
-  "./app.js?v=waypoints-43",
+  "./styles.css?v=waypoints-44",
+  "./app.js?v=waypoints-44",
   "./icon.svg",
   "./manifest.webmanifest",
   "./impressum.html",
