99d8e5a1ebe08f4f25e16b641be598c1b7e37c6f
alex <alex@1hofmann.de>
2026-06-01 07:40:07 +0200

    Make arena button readable

--- s2-niantic-map/sw.js
+++ s2-niantic-map/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "s2-maps-shell-v71";
+const CACHE_NAME = "s2-maps-shell-v72";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=waypoints-68",
-  "./app.js?v=waypoints-68",
+  "./styles.css?v=waypoints-69",
+  "./app.js?v=waypoints-69",
   "./icon.svg",
   "./manifest.webmanifest",
   "./impressum.html",
